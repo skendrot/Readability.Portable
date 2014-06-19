f96d1aa8784639e176f912af7998d76fa989673c
skendrot <skendrot@hotmail.com>
2014-06-18 22:49:57 -0600

    Add ability to get a single article

---
+++ source/Readability.Portable/ServiceMethods/ReadabilityClient-Articles.cs
@@ -0,0 +1,22 @@
+﻿using AsyncOAuth;
+using Newtonsoft.Json;
+using Readability.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Readability
+{
+    public partial class ReadabilityClient
+    {
+        const string ArticleUrl = BaseUrl + "articles";
+
+        public async Task<Article> GetArticlekAsync(int articleId)
+        {
+            string url = string.Format("{0}/{1}", ArticleUrl, articleId);
+            var client = new HttpClient(new OAuthMessageHandler(_consumerKey, _consumerSecret, AccessToken));
+            var json = await client.GetStringAsync(url).ConfigureAwait(false);
+
+            return JsonConvert.DeserializeObject<Article>(json);
+        }
+    }
+}
